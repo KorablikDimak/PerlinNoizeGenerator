9810846819126b65b7bba0863204bbffacc7e8f7
korab <korablikdimak@gmail.com>
2021-09-07 02:04:32 +0300

    normalMaps

--- SphereRotator.cs
+++ SphereRotator.cs
@@ -4,25 +4,33 @@ namespace PerlinNoiseGenerator
 {
     public class SphereRotator : MonoBehaviour
     {
-        [SerializeField] private NoiseMapRenderer noiseMapRenderer;
-
-        private void Update()
+        [Range(0, 1)] [SerializeField] private float speedOfRotation;
+        
+        private void FixedUpdate()
         {
             if (Input.GetKey(KeyCode.A))
             {
-                
+                transform.Rotate(0, speedOfRotation, 0);
             }
             else if (Input.GetKey(KeyCode.D))
             {
-                
+                transform.Rotate(0, -speedOfRotation, 0);
             }
-            else if (Input.GetKey(KeyCode.W))
+            if (Input.GetKey(KeyCode.W))
             {
-                
+                transform.Rotate(0, 0, speedOfRotation);
             }
             else if (Input.GetKey(KeyCode.S))
             {
-                
+                transform.Rotate(0, 0, -speedOfRotation);
+            }
+            if (Input.GetKey(KeyCode.Q))
+            {
+                transform.Rotate(-speedOfRotation, 0, 0);
+            }
+            else if (Input.GetKey(KeyCode.E))
+            {
+                transform.Rotate(speedOfRotation, 0, 0);
             }
         }
     }
